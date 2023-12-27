5a6e4d85e379545db25020cc2bc15641bc045e4e
pap44-ua <pap44@alu.ua.es>
2023-12-27 18:16:54 +0100

    no entiendo

--- client/client-final/src/components/IniciarSesion.vue
+++ client/client-final/src/components/IniciarSesion.vue
@@ -1,30 +1,44 @@
 <template>
   <div>
-    <h2>Iniciar Sesión</h2>
-    <form @submit.prevent="handleLogin">
-      <label for="username">Usuario:</label>
-      <input v-model="username" type="text" required />
-
-      <label for="password">Contraseña:</label>
-      <input v-model="password" type="password" required />
-
-      <button type="submit" :disabled="isLoading">Iniciar Sesión</button>
-      <p v-if="error" style="color: red;">{{ error }}</p>
+    <h1>Iniciar Sesión</h1>
+    <form @submit.prevent="iniciarSesion">
+      <div>
+        <label for="usuario">Nombre de Usuario:</label>
+        <input type="text" id="usuario" v-model="usuario" required>
+      </div>
+      <div>
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" v-model="password" required>
+      </div>
+      <div>
+        <button type="submit">Iniciar Sesión</button>
+      </div>
     </form>
+
+    <!-- Mostrar mensaje de error -->
+    <div v-if="errorMensaje" style="color: red;">
+      {{ errorMensaje }}
+    </div>
   </div>
 </template>
 
 <script>
+<<<<<<< HEAD
 import { useAuthStore } from '../stores/auth';
 import { useRouter } from 'vue-router';
+=======
+import axios from 'axios';
+>>>>>>> parent of c290559 (ns si va)
 
 export default {
   data() {
     return {
-      username: '',
+      usuario: '',
       password: '',
+      errorMensaje: ''
     };
   },
+<<<<<<< HEAD
   setup() {
     const authStore = useAuthStore();
     const router = useRouter();
@@ -51,5 +65,41 @@ export default {
       return this.authStore.$state.error;
     },
   },
+=======
+  methods: {
+    async iniciarSesion() {
+      try {
+        // Realizar la solicitud de inicio de sesión
+        const response = await axios.post('/moderador/login', {
+          Usuario: this.usuario,
+          Contrasena: this.password
+        });
+
+        // Verificar si se recibió un token en la respuesta
+        const token = response.data.token;
+        if (token) {
+          // Almacenar el token en el almacenamiento local (puedes usar cookies o Vuex si es necesario)
+          localStorage.setItem('jwtToken', token);
+
+          // Redirigir a la ruta '/' después de un inicio de sesión exitoso
+          this.$router.push('/');
+        } else {
+          // Manejar el caso en el que no se reciba un token
+          this.errorMensaje = 'El servidor no proporcionó un token válido';
+        }
+      } catch (error) {
+        // Manejar errores de la solicitud aquí
+        console.error('Error en la solicitud:', error.response.data);
+
+        // Mostrar el mensaje de error del servidor si está disponible
+        this.errorMensaje = error.response.data.error || 'Credenciales incorrectas';
+      }
+    }
+  }
+>>>>>>> parent of c290559 (ns si va)
 };
 </script>
+
+<style>
+/* Estilos del componente */
+</style>
--- client/client-final/src/main.js
+++ client/client-final/src/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // main.js
 import { createApp } from 'vue';
 import App from './App.vue';
@@ -15,3 +16,16 @@ const router = createRouter({
 app.use(pinia);
 app.use(router);
 app.mount('#app');
+=======
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  el: '#app',
+  router,
+  render: (h) => h(App),
+});
+>>>>>>> parent of c290559 (ns si va)
